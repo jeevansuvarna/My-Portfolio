6c53e29b9b83f5f2a8260ac8faa24268250fd49a
inno <jeevan.suvarna@ril.com>
2023-12-24 19:38:01 +0530

    2023-removed unndefined import

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,5 @@ import profile from "../assets/images/profile.jpeg"
 import project from "../assets/images/project.png"
 import project1 from "../assets/images/project1.png"
 import project2 from "../assets/images/project2.png"
-import resume from "../assets/files/resume2023.pdf"
 
 export { profile, project, project1, project2 }
