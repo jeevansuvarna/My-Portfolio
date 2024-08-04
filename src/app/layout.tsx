937640578b3b3c22cc0a67716289b050975594a3
jeevan.suvarna <jeevan.suvarna@ril.com>
2024-08-04 20:17:29 +0530

    added clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
         <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "YOUR_CLARITY_PROJECT_ID");
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ni2h9q5q9y");
               `,
             }}
           />
