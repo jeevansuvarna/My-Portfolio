4b8a384b9f7d0eba0755fc6f941a87fea5213d05
Jeevan Suvarna <jeevan.suvarna@ril.com>
2025-07-27 16:34:38 +0530

    project updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,4 +4,6 @@ import project from "../assets/images/project.png"
 import project1 from "../assets/images/project1.png"
 import project2 from "../assets/images/project2.png"
 import project3 from "../assets/images/project3.webp"
-export { profile, project, project1, project2,project3 }
+import project4 from "../assets/images/project4.png"
+
+export { profile, project, project1, project2, project3, project4 }
--- src/component/Projects.tsx
+++ src/component/Projects.tsx
@@ -2,13 +2,61 @@
 
 import Image from 'next/image';
 import Title from './Title';
-import { project1, project2, project3 } from '@/assets';
+import { project1, project2, project3, project4 } from '@/assets';
 import { TbBrandGithub } from 'react-icons/tb';
 import { RxOpenInNewWindow } from 'react-icons/rx';
 import useScrollReveal from '@/hooks/useScrollReveal';
 
 export default function Projects() {
   const { ref, isVisible } = useScrollReveal();
+
+  const projects = [
+    {
+      id: 1,
+      title: 'Portfolio Website',
+      description:
+        'My personal website, inspired by Brittany Chiang. It displays my experiences, skills, and projects.',
+      tech: ['Nextjs', 'TailWind CSS', 'Netify'],
+      image: project2,
+      reverse: true,
+      github: 'https://github.com/jeevansuvarna/portfolio',
+      live: 'https://jeevan-suvarna.netlify.app/',
+    },
+    {
+      id: 2,
+      title: 'Spotilytics',
+      description:
+        '🎧 Built Spotilytics, a React-based web app that connects with your Spotify account to reveal personalized music analytics. 📊 It showcases top tracks, favorite artists, playlists, and visual charts based on your listening habits. 🚀 Designed with a sleek and responsive UI to make exploring your music journey both fun and insightful.',
+      tech: ['React', 'Spotify API', 'Netify', 'CSS'],
+      image: project4,
+      reverse: false,
+      github: 'https://github.com/jeevansuvarna/Spotilytics',
+      live: 'https://spotilytics.netlify.app/',
+    },
+    {
+      id: 3,
+      title: 'One For All Sort 💥⚡',
+      description:
+        'An application that visually illustrates well-known sorting algorithms such as merge sort, quick sort, and so on in action using bar graphs.',
+      tech: ['React js'],
+      image: project1,
+      reverse: true,
+      github: 'https://github.com/jeevansuvarna/SortingVisualizer',
+      live: 'https://sort-algoviz.netlify.app/',
+    },
+    {
+      id: 4,
+      title: 'Raijin - CLI Shortcut Manager ⚡🥷',
+      description:
+        'Raijin is a command-line tool that allows users to create and execute shortcuts for frequently used commands. It simplifies workflow automation by storing shortcuts in a JSON config file.',
+      tech: ['Go'],
+      image: project3,
+      reverse: false,
+      github: 'https://github.com/jeevansuvarna/Raijin-CLI-Shortcut',
+      live: 'https://drive.google.com/file/d/1uACUbO3cDhAtnaCvvDOr0-4B9h4DrFt3/view?usp=sharing',
+    },
+  ];
+
   return (
     <section
       id='project'
@@ -19,167 +67,79 @@ export default function Projects() {
     >
       {' '}
       <Title title='Some Things I have Built' id='03' />
-      <div className='w-full flex gap-28 flex-col items-center justify-between'>
-        {/* First Project use loop */}
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row-reverse gap-6'>
-            <a
-              href='/'
-              target='_blank'
-              className='w-full xl:w-1/2 h-auto  group'
+      {
+        <div className='w-full flex gap-28 flex-col items-center justify-between'>
+          {projects.map((project, index) => (
+            <div
+              key={project.id}
+              className='w-full flex flex-col items-center justify-center gap-28 mt-10'
             >
-              <div className='relative'>
-                <Image
-                  className='w-full h-full object-contain border border-[var(--text-underline)] rounded-md'
-                  src={project2}
-                  alt='project'
-                />
-                <div className='hidden lgl:inline-block absolute w-full h-full bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300'></div>
-              </div>
-            </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right'>
-              <p className='font-titleFont text-textGreen text-sm tracking-wide'>
-                Featured Project
-              </p>
-              <h3 className='text-2xl font-bold'>Portfolio Website</h3>
-              <p className=' bg-bodyColor relative z-20 text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md shadow-shadowColor'>
-                My personal website , inspired from brittany chiang. Which
-                displays my experiences , skills and projects.
-              </p>
-              <ul className='flex gap-2 text-xs md:text-sm font-titleFont tracking-wide md:gap-5 justify-between text-textDark left-0'>
-                <li>Nextjs</li>
-                <li>TailWind CSS</li>
-              </ul>
-              <div className='flex gap-6 text-2xl'>
+              <div
+                className={`flex flex-col ${
+                  project.reverse ? 'xl:flex-row-reverse' : 'xl:flex-row'
+                } gap-6`}
+              >
+                {/* Image */}
                 <a
-                  href='/'
-                  className='hover:text-textGreen duration-300'
+                  href={project.live}
                   target='_blank'
+                  rel='noreferrer'
+                  className='w-full xl:w-1/2 h-auto group'
                 >
-                  <TbBrandGithub />
+                  <div className='relative'>
+                    <Image
+                      className='w-full h-full object-contain border border-[var(--text-underline)] rounded-md max-h-[332px]'
+                      src={project.image}
+                      alt={project.title}
+                    />
+                    <div className='hidden lgl:inline-block absolute w-full h-full bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300'></div>
+                  </div>
                 </a>
-                <a
-                  href='/'
-                  className='hover:text-textGreen duration-300'
-                  target='_blank'
-                >
-                  <RxOpenInNewWindow />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Second Project use loop */}
 
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row gap-6'>
-            <a
-              href='https://sort-algoviz.netlify.app/'
-              target='_blank'
-              className='w-full xl:w-1/2 h-auto group'
-            >
-              <div className='relative'>
-                <div className='hidden lgl:inline-block absolute w-full h-full bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300'></div>
-                <Image
-                  className='w-full h-full object-contain border border-[var(--text-underline)] rounded-md'
-                  src={project1}
-                  alt='project'
-                />
-              </div>
-            </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16'>
-              <p className='font-titleFont text-textGreen text-sm tracking-wide'>
-                Featured Project
-              </p>
-              <h3 className='text-2xl font-bold'>One For All Sort 💥⚡</h3>
-              <p className=' bg-bodyColor relative z-20 text-sm md:text-base p-2 md:p-6 rounded-md shadow-shadowColor'>
-                An application that visually illustrate well-know{' '}
-                <span className='text-textGreen underline-transition text-textUnderline'>
-                  sorting algorithm
-                </span>{' '}
-                such as merge sort , quick sort and so on in action using bar
-                graphs.
-              </p>
-              <ul className='flex gap-2 text-xs md:text-sm font-titleFont tracking-wide md:gap-5 justify-between text-textDark'>
-                <li>React js</li>
-              </ul>
-              <div className='flex gap-6 text-2xl '>
-                <a
-                  href='https://github.com/jeevansuvarna/SortingVisualizer'
-                  className='hover:text-textGreen duration-300'
-                  target='_blank'
+                {/* Content */}
+                <div
+                  className={`w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right `}
                 >
-                  <TbBrandGithub />
-                </a>
-                <a
-                  href='https://sort-algoviz.netlify.app/'
-                  className='hover:text-textGreen duration-300'
-                  target='_blank'
-                >
-                  <RxOpenInNewWindow />
-                </a>
+                  <p className='font-titleFont text-textGreen text-sm tracking-wide'>
+                    Featured Project
+                  </p>
+                  <h3 className='text-2xl font-bold'>{project.title}</h3>
+                  <p
+                    className={`bg-bodyColor relative z-20 text-sm md:text-base p-2 md:p-6 rounded-md shadow-shadowColor ${
+                      project.reverse ? 'xl:-mr-16' : 'xl:-ml-16'
+                    }`}
+                  >
+                    {project.description}
+                  </p>
+                  <ul className='flex gap-2 text-xs md:text-sm font-titleFont tracking-wide md:gap-5 justify-between text-textDark'>
+                    {project.tech.map((tech, i) => (
+                      <li key={i}>{tech}</li>
+                    ))}
+                  </ul>
+                  <div className='flex gap-6 text-2xl'>
+                    <a
+                      href={project.github}
+                      target='_blank'
+                      rel='noreferrer'
+                      className='hover:text-textGreen duration-300'
+                    >
+                      <TbBrandGithub />
+                    </a>
+                    <a
+                      href={project.live}
+                      target='_blank'
+                      rel='noreferrer'
+                      className='hover:text-textGreen duration-300'
+                    >
+                      <RxOpenInNewWindow />
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-
-        {/* Third Project use loop */}
-        <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-          <div className='flex flex-col xl:flex-row-reverse gap-6'>
-            <a
-              href='/'
-              target='_blank'
-              className='w-full xl:w-1/2 h-auto  group'
-            >
-              <div className='relative'>
-                <Image
-                  className='w-full h-full object-contain max-h-[332px] border border-[var(--text-underline)] rounded-md'
-                  src={project3}
-                  alt='project'
-                />
-                <div className='hidden lgl:inline-block absolute w-full h-full bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300'></div>
-              </div>
-            </a>
-            <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right'>
-              <p className='font-titleFont text-textGreen text-sm tracking-wide'>
-                Featured Project
-              </p>
-              <h3 className='text-2xl font-bold'>
-                Raijin - CLI Shortcut Manager
-              </h3>
-              <p className=' bg-bodyColor relative z-20 text-sm md:text-base p-2 xl:-mr-16 md:p-6 rounded-md shadow-shadowColor'>
-                ⚡🥷{' '}
-                <span className='text-textGreen underline-transition text-textUnderline'>
-                  Raijin{' '}
-                </span>
-                is a command-line tool that allows users to create and execute
-                shortcuts for frequently used commands. It simplifies workflow
-                automation by storing shortcuts in a JSON configuration file
-                located on the Desktop for easy access and modification.
-              </p>
-              <ul className='flex gap-2 text-xs md:text-sm font-titleFont tracking-wide md:gap-5 justify-between text-textDark left-0'>
-                <li>Go</li>
-              </ul>
-              <div className='flex gap-6 text-2xl '>
-                <a
-                  href='https://github.com/jeevansuvarna/Raijin-CLI-Shortcut'
-                  className='hover:text-textGreen duration-300'
-                  target='_blank'
-                >
-                  <TbBrandGithub />
-                </a>
-                <a
-                  href='https://drive.google.com/file/d/1uACUbO3cDhAtnaCvvDOr0-4B9h4DrFt3/view?usp=sharing'
-                  className='hover:text-textGreen duration-300'
-                  target='_blank'
-                >
-                  <RxOpenInNewWindow />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      }
     </section>
   );
 }
