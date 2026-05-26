349e57b4091ae479336d2fadf50f60fdcd481a8c
Jeevan Suvarna <jeevansuvarna71@gmail.com>
2026-05-26 09:56:49 +0530

    Image changed

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
 
-import profile from "../assets/images/profile.jpeg"
+import profile from "../assets/images/profile.jpg"
 import project from "../assets/images/project.png"
 import project1 from "../assets/images/project1.png"
 import project2 from "../assets/images/project2.png"
